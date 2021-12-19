02692380860c2ae739a509fefe39d94f945f4512
Hazue <daishizennoha@gmail.com>
2021-12-19 17:42:47 +0800

    ignore log files; improve messages in la-rts.py

--- la-rts.py
+++ la-rts.py
@@ -2,49 +2,81 @@ from selenium import webdriver
 import time
 import sys
 import os
+import logging
 
 
 def validateRequiredExecutables():
     filesInThisFolder = [f for f in os.listdir('.') if os.path.isfile(f)]
     
     if "ffmpeg.exe" not in filesInThisFolder:
-        print('Please place ffmpeg.exe in the same folder as this python file')
-        return False
+        print('Cannot find ffmpeg.exe.')
+        print('Please place ffmpeg.exe in the same folder as this python file.')
+        quit()
         
     if "chromedriver.exe" not in filesInThisFolder:
-        print('Please place chromedriver.exe in the same folder as this python file')
-        return False
+        print('Cannot find chromedriver.exe.')
+        print('Please place chromedriver.exe in the same folder as this python file.')
+        quit()
         
-    return True
+
+def validateUrlKeyword(strKeyword):
+    if strKeyword not in strTwitterUrl:
+        print("Hmmmmm this URL has no ["+ strKeyword +"] :thonk: ")
+        print("Exitting this script lol")
+        exit()
 
 
 if __name__ == '__main__':
     
-    if validateRequiredExecutables() is False:
-        quit()
+    ###################################
+    ########### Validations ###########
+    ###################################
     
+    # will quit() inside this function
+    validateRequiredExecutables()
+    
+    # make is legit URL 
     strTwitterUrl = sys.argv[1]
+    if len(strTwitterUrl) <= 10:
+        print("Is this even a legit URL for Twitter Space? Exitting lol")
+        exit()
     
-    print("Preparing javascript to inject")
+    validateUrlKeyword('twitter')
+    validateUrlKeyword('https')
+    
+    
+    
+    ###################################
+    ########### Preparation ###########
+    ###################################
+    
+    print("\n1. Preparing javascript to inject")
     jsfile = open("tmp-get-twitter-space-m3u8.js", "r")
     strJavaScript = jsfile.read()
     time.sleep(1)
     
+    
+    
+    
+    ###################################
+    ############ Real Work ############
+    ###################################
+    
     # TODO: add function to auto detect chromedriver regardless of version
-    print("Opening [controlled browser]")
+    print("\n2. Opening [controlled browser]")
     driver = webdriver.Chrome(executable_path="chromedriver.exe")
     time.sleep(1)
     
-    print('Loading twitter space. Assume it takes 5 seconds to load')
+    print('\n3. Loading twitter space. Assume it takes 5 seconds to load')
     driver.get(strTwitterUrl)
     time.sleep(5)
     
-    print("Getting m3u8 URL for ffmpeg")
-    print("Takes about 10 seconds to observe twitter's traffic.")
+    print("\n4. Getting m3u8 URL for ffmpeg")
+    print("(Takes about 10 seconds to observe twitter's traffic)")
     driver.execute_script(strJavaScript)
     time.sleep(10)
     
-    print("10 seconds passed. hopefully we capture the m3u8 URL.")
+    print("\n5. 10 seconds passed. hopefully we capture the m3u8 URL.")
     strUrlM3U8 = driver.execute_script('return strPlaylistm3u8')
     
     if strUrlM3U8 is None or strUrlM3U8 == '':
@@ -52,21 +84,19 @@ if __name__ == '__main__':
         print("Hazue will debug when he's free")
         exit()
     
-    print("Ayyy we got m3u8 URL, time to run ffmpeg")
+    print("\n6. Ayyy we got the m3u8 URL. The script will auto run ffmpeg")
     strFullCommand = 'ffmpeg -user_agent "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/601.7.8 (KHTML, like Gecko) Version/9.1.3 Safari/537.86.7" '
     strFullCommand += ' -i '+strUrlM3U8
     strFullCommand += ' -c copy space.m4a'
-    
-    
-    print("\nCopy paste below command, run it and the downloading will begins.\n")
+
     print(strFullCommand)
     time.sleep(3)
     
-    print("\nRunning command in 3 seconds\n")
+    print("\n7. Running ffmpeg command in 3 seconds\n")
     os.system(strFullCommand)
     
-    print("\n\nThis program isn't fool-proof so good luck in downloading (may the force be with you)\n")
-    input("Completed! press enter to exit")
+    input("\n\nCompleted! press enter to exit")
+    print("exitting... closing browser...")
     
     driver.close()
     driver.quit()
